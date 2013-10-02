5d0dff4002b724bb36d2232ce886149c83dbfc3b
Ming Xiao <ming.xiao@oxygencloud.com>
2013-10-02 12:22:13 -0700

    working on 'move'

--- my_webdav.py
+++ my_webdav.py
@@ -13,15 +13,15 @@ def get_session(username, password):
     return sess
 
 
-def download(session,filename, host, protocol='https'):
+def download(session,filename, host, downloadPath, protocol='https'):
     r = session.get("{}://{}/{}".format(protocol,host,filename),stream=True)
     if r.status_code == 200:
         print 'downloading...'
-        path = os.path.join(os.getcwd(),'other.jpg')
-        with open(path,'wb') as f:
+        #path = os.path.join(os.getcwd(),'other.jpg')
+        with open(downloadPath,'wb') as f:
             for chunk in r.iter_content():
                 f.write(chunk)
-        print 'Downloaded {} to {}'.format(filename, path)
+        print 'Downloaded {} to {}'.format(filename, downloadPath)
     else:
         print 'Failed to download {}'.format(filename)
 
@@ -38,32 +38,44 @@ def upload(session, filePath, serverPath, host, protocol = 'https'):
 
 def delete(session, filename,host,protocol='https'):
     response = session.delete("{}://{}/{}".format(protocol,host,filename))
-    if str(response.status_code).startswith('2'):
+    if good_status(response.status_code):
         print 'Deleted {}'.format(filename)
     else:
         print 'Failed to delete {}'.format(filename)
 
 def addFolder(session, folderPath, host, user, password, protocol = 'https'):
-    #header = {"Authorization": "Basic testwebdav:password1"}
     base64string = base64.encodestring('{}:{}'.format(user, password))[:-1]
     myHeader = {"Depth": 1, "authorization":"Basic %s" % base64string}
     prepped = requests.Request('MKCOL', "{}://{}/{}".format(protocol, host,folderPath), headers=myHeader).prepare()
-    print prepped.url
     response = session.send(prepped)
-    print response.status_code, response.content
+    if good_status(response.status_code):
+        print 'Successfully created folder {}'.format(folderPath)
+    else:
+        print 'Failed to create folder {}'.format(folderPath)
 
-def ls(session, path):
+def ls(session, path, host, protocol = 'https'):
     """
     We use the PROPFIND command
     """
     base64string = base64.encodestring('%s:%s' % ('testwebdav', 'password1'))[:-1]  # remove ending newline
     myHeader = {"Depth": 1, "authorization":"Basic %s" % base64string}
-    prepped = requests.Request('PROPFIND', "https://webdav.o2cloud.net/%s" % path, headers = myHeader).prepare()
+    prepped = requests.Request('PROPFIND', "{}://{}/{}".format(protocol, host, path), headers = myHeader).prepare()
     response = session.send(prepped)
     if good_status(response.status_code):
         print response.content
     else:
         print 'Failed to list {}'.format(path)
+
+def move(session, src, dest, host, protocol = 'https'):
+    prepped_src = os.path.join('{}://{}/'.format(protocol,host),src)
+    prepped_dest = os.path.join('{}://{}/'.format(protocol,host),dest)
+    myHeader = {"Destination":prepped_dest, "Host":host}
+    prepped = requests.Request('MOVE', prepped_src, headers = myHeader).prepare()
+    response = session.send(prepped)
+    if good_status(response.status_code):
+        print 'Successfully moved {} to {}'.format(src, dest)
+    else:
+        print 'Failed to move {} to {}'.format(src, dest)
 
 
 def good_status(status_code):
@@ -75,34 +87,44 @@ def good_status(status_code):
 
 def extract(xmlstr, np, tag):
     root = ET.fromstring(xmlstr)
-    for f in root.findall(tag, namespaces= np):
+    for f in root.findall(tag, namespaces=np):
         print 'here'
         print f
 
 
-user = 'testwebdav'
-password = 'password1'
-host = 'webdav.o2cloud.net'
-sess = get_session(user, password)
+if __name__ == '__main__':
+    user = 'testwebdav'
+    password = 'password1'
+    host = 'webdav.o2cloud.net'
+    sess = get_session(user, password)
 
+    #download - file
+    #filename = '00546_lonelybeach_2560x1600.jpg'
+    #downPath = os.path.join(os.getcwd(),filename)
+    #download(sess, filename, host, downPath)
 
-#download
-#filename = '00546_lonelybeach_2560x1600.jpg'
-#download(sess, filename, host)
+    #upload
+    #upfile = os.path.join(os.getcwd(), 'foo.txt')
+    #serverFile = 'somefile.txt'
+    #upload(sess, upfile, serverFile, host)
 
-#upload
-#upfile = os.path.join(os.getcwd(), 'foo.txt')
-#serverFile = 'somefile.txt'
-#upload(sess, upfile, serverFile, host)
+    #delete - file
+    #filename = 'somefile'
+    #delete(sess, filename, host)
 
-#delete
-#filename = 'somefile.txt'
-#delete(sess,filename)
+    #add folder
+    #foldername= 'my_folder'
+    #addFolder(sess, foldername, host, user, password)
 
-#add folder
-#foldername= 'my_folder'
-#addFolder(sess, foldername, host, user, password)
+    #delete - folder
+    #folder_name = 'my_folder'
+    #delete(sess,folder_name, host)
 
-#ls
-#folder = ''
-#ls(sess,folder)
+    #ls
+    #folder = ''
+    #ls(sess,folder, host)
+
+    #move
+    src = '/somefile.txt'
+    dest = '/my_folder'
+    move(sess, src, dest, host)
